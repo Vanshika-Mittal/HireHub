e5f01839603a3d0844bda0aaa7e2ad1f4cd347da
Amulya Kolar <amulyakolar@gmail.com>
2025-04-16 10:56:22 +0530

    added auth base files

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,5 @@
+﻿function Home () {
+    return <div>Home</div>
+}
+
+export default Home
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,7 @@
+﻿import Form from "../components/Form"
+
+function Register() {
+    return <Form route="/api/user/register/" method="register" />
+}
+
+export default Register
